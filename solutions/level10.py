47d90f2a592abc833bbfb68da4d2aa1d3d5b132c
Dominik "Dodo" Dopplinger <dom.dopplinger@gmail.com>
2023-12-10 20:38:19 +0100

    Refactoring level 10

--- solutions/level10.py
+++ solutions/level10.py
@@ -3,8 +3,8 @@ from enum import Enum
 from util.file_util import read_input_file_id
 
 
-class CantStepException(Exception):
-    pass
+class CantStepException(Exception): pass
+class InvalidDirectionsException(Exception): pass
 
 
 class LoopDirection(Enum):
@@ -37,25 +37,25 @@ class Labyrinth:
         self.pipes = definition
         self.direction_translate = {
             Direction.North: {
-                "S": Direction.North,
+                "S": None,
                 "|": Direction.North,
                 "7": Direction.West,
                 "F": Direction.East,
             },
             Direction.East: {
-                "S": Direction.East,
+                "S": None,
                 "-": Direction.East,
                 "J": Direction.North,
                 "7": Direction.South,
             },
             Direction.South: {
-                "S": Direction.South,
+                "S": None,
                 "|": Direction.South,
                 "L": Direction.East,
                 "J": Direction.West,
             },
             Direction.West: {
-                "S": Direction.West,
+                "S": None,
                 "-": Direction.West,
                 "L": Direction.North,
                 "F": Direction.South,
@@ -74,17 +74,17 @@ class Labyrinth:
     def can_step(self, x, y, direction) -> bool:
         new_x, new_y = direction.step(x, y)
         if direction == Direction.North and new_y != -1:
-            value = self.pipes[new_y][new_x]
-            return value == "S" or value != "|" or value != "7" or value != "F"
+            new_value = self.pipes[new_y][new_x]
+            return new_value == "S" or new_value != "|" or new_value != "7" or new_value != "F"
         elif direction == Direction.East and new_x != len(self.pipes[0]):
-            value = self.pipes[new_y][new_x]
-            return value == "S" or value != "-" or value != "J" or value != "7"
+            new_value = self.pipes[new_y][new_x]
+            return new_value == "S" or new_value != "-" or new_value != "J" or new_value != "7"
         elif direction == Direction.South and new_y != len(self.pipes):
-            value = self.pipes[new_y][new_x]
-            return value == "S" or value != "|" or value != "L" or value != "J"
+            new_value = self.pipes[new_y][new_x]
+            return new_value == "S" or new_value != "|" or new_value != "L" or new_value != "J"
         elif direction == Direction.West and new_x != -1:
-            value = self.pipes[new_y][new_x]
-            return value == "S" or value != "-" or value != "L" or value != "F"
+            new_value = self.pipes[new_y][new_x]
+            return new_value == "S" or new_value != "-" or new_value != "L" or new_value != "F"
         return False
 
 
@@ -102,117 +102,138 @@ class LoopTile:
         self.new_direction = new_direction
         self.value = value
 
+    def get_loop_direction(self) -> LoopDirection | None:
+        direction_diff = self.new_direction.value - self.old_direction.value
+
+        if direction_diff == 1 or direction_diff == -3:
+            return LoopDirection.Right
+        elif direction_diff == -1 or direction_diff == 3:
+            return LoopDirection.Left
+        else:
+            return None
+
+    def get_fill_tiles(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if self.value == "|":
+            return self.__get_fill_tiles_for_bar(loop_direction)
+        elif self.value == "-":
+            return self.__get_fill_tiles_for_minus(loop_direction)
+        elif self.value == "L":
+            return self.__get_fill_tiles_for_l(loop_direction)
+        elif self.value == "J":
+            return self.__get_fill_tiles_for_j(loop_direction)
+        elif self.value == "7":
+            return self.__get_fill_tiles_for_7(loop_direction)
+        elif self.value == "F":
+            return self.__get_fill_tiles_for_f(loop_direction)
+        elif self.value == "S":
+            return self.__get_fill_tiles_for_s(loop_direction)
+        return []
+
+    def __get_fill_tiles_for_bar(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if ((loop_direction == LoopDirection.Right and self.new_direction == Direction.North)
+                or (loop_direction == LoopDirection.Left and self.new_direction == Direction.South)):
+            return [(1, 0)]
+        else:
+            return [(-1, 0)]
+
+    def __get_fill_tiles_for_minus(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if ((loop_direction == LoopDirection.Right and self.new_direction == Direction.West)
+                or (loop_direction == LoopDirection.Left and self.new_direction == Direction.East)):
+            return [(0, -1)]
+        else:
+            return [(0, 1)]
+
+    def __get_fill_tiles_for_l(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if ((loop_direction == LoopDirection.Right and self.new_direction == Direction.East)
+                or (loop_direction == LoopDirection.Left and self.new_direction == Direction.North)):
+            return [(0, 1), (-1, 0)]
+        else:
+            return []
+
+    def __get_fill_tiles_for_j(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if ((loop_direction == LoopDirection.Right and self.new_direction == Direction.North)
+                or (loop_direction == LoopDirection.Left and self.new_direction == Direction.West)):
+            return [(0, 1), (1, 0)]
+        else:
+            return []
+
+    def __get_fill_tiles_for_7(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if ((loop_direction == LoopDirection.Right and self.new_direction == Direction.West)
+                or (loop_direction == LoopDirection.Left and self.new_direction == Direction.South)):
+            return [(0, -1), (1, 0)]
+        else:
+            return []
+
+    def __get_fill_tiles_for_f(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if ((loop_direction == LoopDirection.Right and self.new_direction == Direction.South)
+                or (loop_direction == LoopDirection.Left and self.new_direction == Direction.East)):
+            return [(0, -1), (-1, 0)]
+        else:
+            return []
+
+    def __get_fill_tiles_for_s(self, loop_direction: LoopDirection) -> list[tuple[int, int]]:
+        if loop_direction == LoopDirection.Right:
+            if self.new_direction == Direction.North:
+                return [(1, 0)]
+            elif self.new_direction == Direction.East:
+                return [(0, 1)]
+            elif self.new_direction == Direction.South:
+                return [(-1, 0)]
+            else:
+                return [(0, -1)]
+        else:
+            if self.new_direction == Direction.North:
+                return [(-1, 0)]
+            elif self.new_direction == Direction.East:
+                return [(0, -1)]
+            elif self.new_direction == Direction.South:
+                return [(1, 0)]
+            else:
+                return [(0, 1)]
+
 
 class FillLabyrinth:
     area: list[list[str]]
+    loop_tiles: list[LoopTile]
     loop_direction: LoopDirection
-    fill_tiles: dict[LoopDirection, dict[str, dict[Direction, list[tuple[int, int]]]]]
 
     __clear_tile = "."
     __inside_tile = "*"
 
     def __init__(self, labyrinth: Labyrinth, loop_tiles: list[LoopTile]):
         self.area = [[self.__clear_tile for _ in labyrinth.pipes[0]] for _ in labyrinth.pipes[0]]
+        self.loop_tiles = loop_tiles
 
-        self.fill_tiles = {
-            LoopDirection.Right: {
-                "|": {
-                    Direction.North: [(1, 0)],
-                    Direction.South: [(-1, 0)],
-                },
-                "-": {
-                    Direction.West: [(0, -1)],
-                    Direction.East: [(0, 1)],
-                },
-                "L": {
-                    Direction.North: [],
-                    Direction.East: [(0, 1), (-1, 0)],
-                },
-                "J": {
-                    Direction.North: [(0, 1), (1, 0)],
-                    Direction.West: [],
-                },
-                "7": {
-                    Direction.South: [],
-                    Direction.West: [(0, -1), (1, 0)],
-                },
-                "F": {
-                    Direction.East: [],
-                    Direction.South: [(0, -1), (-1, 0)],
-                },
-                "S": {
-                    Direction.North: [(1, 0)],
-                    Direction.East: [(0, 1)],
-                    Direction.South: [(-1, 0)],
-                    Direction.West: [(0, -1)],
-                }
-            },
-            LoopDirection.Left: {
-                "|": {
-                    Direction.South: [(1, 0)],
-                    Direction.North: [(-1, 0)],
-                },
-                "-": {
-                    Direction.East: [(0, -1)],
-                    Direction.West: [(0, 1)],
-                },
-                "L": {
-                    Direction.East: [],
-                    Direction.North: [(0, 1), (-1, 0)],
-                },
-                "J": {
-                    Direction.West: [(0, 1), (1, 0)],
-                    Direction.North: [],
-                },
-                "7": {
-                    Direction.West: [],
-                    Direction.South: [(0, -1), (1, 0)],
-                },
-                "F": {
-                    Direction.South: [],
-                    Direction.East: [(0, -1), (-1, 0)],
-                },
-                "S": {
-                    Direction.South: [(1, 0)],
-                    Direction.West: [(0, 1)],
-                    Direction.North: [(-1, 0)],
-                    Direction.East: [(0, -1)],
-                }
-            }
-        }
-
-        num_right_turns = 0
-        num_left_turns = 0
         for loop_tile in loop_tiles:
             self.area[loop_tile.y][loop_tile.x] = loop_tile.value
 
-            if loop_tile.value == "S":
-                direction_diff = loop_tiles[0].new_direction.value - loop_tiles[-1].new_direction.value
-            else:
-                direction_diff = loop_tile.new_direction.value - loop_tile.old_direction.value
-            if direction_diff == 1 or direction_diff == -3:
-                num_right_turns += 1
-            elif direction_diff == -1 or direction_diff == 3:
-                num_left_turns += 1
+        self.loop_direction = self.__calc_loop_direction()
 
-        if num_right_turns > num_left_turns:
-            self.loop_direction = LoopDirection.Right
-        else:
-            self.loop_direction = LoopDirection.Left
-
-        for loop_tile in loop_tiles:
-            to_fill = self.fill_tiles[self.loop_direction][loop_tile.value][loop_tile.new_direction]
+        for loop_tile in loop_tiles[0:-1]:
+            to_fill = loop_tile.get_fill_tiles(self.loop_direction)
             for field in to_fill:
-                self.flood_fill_inside(loop_tile.x + field[0], loop_tile.y + field[1])
+                self.__flood_fill_inside(loop_tile.x + field[0], loop_tile.y + field[1])
 
-    def flood_fill_inside(self, x: int, y: int):
+    def __calc_loop_direction(self) -> LoopDirection:
+        num_turns = {direction: 0 for direction in LoopDirection}
+        num_turns[None] = 0
+
+        for loop_tile in self.loop_tiles:
+            num_turns[loop_tile.get_loop_direction()] += 1
+
+        if num_turns[LoopDirection.Right] > num_turns[LoopDirection.Left]:
+            return LoopDirection.Right
+        else:
+            return LoopDirection.Left
+        
+    def __flood_fill_inside(self, x: int, y: int):
         if not self.should_flood_fill(x, y):
             return
         self.area[y][x] = self.__inside_tile
-        self.flood_fill_inside(x - 1, y)
-        self.flood_fill_inside(x + 1, y)
-        self.flood_fill_inside(x, y + 1)
-        self.flood_fill_inside(x, y - 1)
+        self.__flood_fill_inside(x - 1, y)
+        self.__flood_fill_inside(x + 1, y)
+        self.__flood_fill_inside(x, y + 1)
+        self.__flood_fill_inside(x, y - 1)
 
     def should_flood_fill(self, x: int, y: int):
         return 0 <= x < len(self.area[0]) and 0 <= y < len(self.area) and self.area[y][x] == self.__clear_tile
@@ -223,37 +244,38 @@ class FillLabyrinth:
 
 def level10(file_id: int) -> tuple[int, int]:
     labyrinth = parse_input_file(file_id)
-    found_s = list(map(lambda line: line.find("S"), labyrinth.pipes))
-    start_y = [idx for idx, value in enumerate(found_s) if value != -1][0]
-    start_x = found_s[start_y]
-    loop_tiles = None
-    longest_distance = -1
-    for start_direction in Direction:
-        x = start_x
-        y = start_y
-        test_loop_tiles: list[LoopTile] = [LoopTile(x, y, None, start_direction, labyrinth.pipes[y][x])]
-        direction = start_direction
-        for num_steps in range(0, 1000000):
-            try:
-                new_x, new_y, new_direction = labyrinth.step(x, y, direction)
-                old_direction = direction
-                x, y, direction = new_x, new_y, new_direction
-                test_loop_tiles.append(LoopTile(x, y, old_direction, direction, labyrinth.pipes[y][x]))
-                if x == start_x and y == start_y:
-                    loop_tiles = test_loop_tiles
-                    longest_distance = int((num_steps + 1) / 2)
-                    break
-            except CantStepException:
-                break
-        if longest_distance != -1:
-            break
+    loop_tiles = calc_loop(labyrinth)
     fill_labyrinth = FillLabyrinth(labyrinth, loop_tiles)
-    return longest_distance, fill_labyrinth.get_num_tiles_inside()
+    return int((len(loop_tiles)) / 2), fill_labyrinth.get_num_tiles_inside()
 
 
 def parse_input_file(file_id: int) -> Labyrinth:
     lines = read_input_file_id(10, file_id)
     return Labyrinth(lines)
+
+
+def calc_loop(labyrinth: Labyrinth) -> list[LoopTile]:
+    start_x, start_y = find_start(labyrinth)
+    for start_direction in Direction:
+        x, y, direction = start_x, start_y, start_direction
+        loop_tiles = []
+        for _ in range(0, 100000):
+            try:
+                old_direction = direction
+                x, y, direction = labyrinth.step(x, y, direction)
+                if labyrinth.pipes[y][x] == "S":
+                    loop_tiles.append(LoopTile(x, y, old_direction, start_direction, labyrinth.pipes[y][x]))
+                    return loop_tiles
+                loop_tiles.append(LoopTile(x, y, old_direction, direction, labyrinth.pipes[y][x]))
+            except CantStepException:
+                break
+
+
+def find_start(labyrinth: Labyrinth) -> tuple[int, int]:
+    found_s = list(map(lambda line: line.find("S"), labyrinth.pipes))
+    start_y = [idx for idx, value in enumerate(found_s) if value != -1][0]
+    start_x = found_s[start_y]
+    return start_x, start_y
 
 
 if __name__ == '__main__':
@@ -265,3 +287,4 @@ def test_level10():
     assert (8, 1) == level10(1)
     assert (23, 4) == level10(2)
     assert (70, 8) == level10(3)
+    assert (6768, 351) == level10(4)
